e9ca8820f8b775f3455472b449193fb6829eb2ff
Chris Hibler <chris@Chriss-MacBook-Pro.local>
2024-09-01 06:17:38 -0700

    feat: add login

--- src/webclient/src/Components/NavBar.jsx
+++ src/webclient/src/Components/NavBar.jsx
@@ -1,7 +1,12 @@
 import "./NavBar.css";
 import { Link } from "react-router-dom";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function NavBar({ children }) {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <nav>
       {children}
@@ -11,6 +16,10 @@ export default function NavBar({ children }) {
         </li>
         <li>
           <Link to="/map">Map</Link>
+        </li>
+        <li>
+          {!isAuthenticated && <LoginButton />}
+          {isAuthenticated && <LogoutButton />}
         </li>
       </ul>
     </nav>
