0eccdc3c256688dc82551afee4b78e345b1e1ce0
Chris Hibler <chrishibler@gmail.com>
2024-11-02 04:29:30 -0700

    feat: navigate on save success

--- src/app/flutter/convention_list/lib/pages/add_page.dart
+++ src/app/flutter/convention_list/lib/pages/add_page.dart
@@ -5,6 +5,7 @@ import 'package:convention_list/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:go_router/go_router.dart';
 
 import '../services/api.dart';
 
@@ -42,6 +43,8 @@ class _AddPageState extends State<AddPage> {
                         ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(content: Text('Some fields are invalid.')),
                         );
+                      } else {
+                        context.pop();
                       }
                     }
                   } catch (e) {
