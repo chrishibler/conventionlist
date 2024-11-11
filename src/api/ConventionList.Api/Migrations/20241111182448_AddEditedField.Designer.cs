55663db57b74b8eec8de7c78ad6825bc47cc2012
Chris Hibler <chrishibler@gmail.com>
2024-11-11 10:26:26 -0800

    fix: add Edited field in migration

--- src/api/ConventionList.Api/Migrations/20241111182448_AddEditedField.Designer.cs
+++ src/api/ConventionList.Api/Migrations/20241111182448_AddEditedField.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ConventionList.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ConventionList.Api.Migrations
 {
     [DbContext(typeof(ConventionListDbContext))]
-    partial class ConventionListDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241111182448_AddEditedField")]
+    partial class AddEditedField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
